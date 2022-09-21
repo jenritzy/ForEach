4d7cd36cf19f940277e0786e1bf1937f6370215a
jenritzy <Jenritzy@outlook.com>
2022-09-21 13:02:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ForEach";
+            string[] websites = new string[5]
+                {"Google","Youtube","Facebook","Baidu","Yahoo!"};
+            Console.WriteLine("Popular Websites...");
+
+            int rank = 1;
+            foreach(string site in websites)
+            {
+                Console.WriteLine("Position:" + rank + "\t" + site);
+                rank++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
